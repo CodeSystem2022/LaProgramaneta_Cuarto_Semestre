8e514355073aabd774ce678f98c5a6b6f878652c
Joaquin Clavell <gonzaloclavell16@gmail.com>
2023-09-27 19:54:55 -0300

    Semana 15 (Python)
    Semana 15 (Python)

---
+++ Laboratorio IV/Python/Semana15_cuarto_semestre/Leccion05/Clase01/Sistemas_numeracion.py
@@ -0,0 +1,39 @@
+# Profundizando en los sistemas de numeración
+
+# Sistema decimal
+a = 10
+print(f'a decimal: {a}')
+
+# Sistema binario
+a = 0b1010
+print(f'a binario: {a}')
+
+# Sistema octal
+a = 0o12
+print(f'a octal: {a}')
+
+# Sistema hexadecimal
+a = 0xA
+print(f'a hexadecimal: {a}')
+
+# Base Decimal
+a = int('23', 10)
+print(f'a = base decimal: {a}')
+
+# Base Binario
+a = int('10111', 2)
+print(f'a = base binario: {a}')
+
+# base octal
+a = int('27',8)
+print(f"a base  octal:{a}")
+
+# base hexadecimal
+a = int('17',16)
+print(f"a la base  hexadecimal:{a}")
+
+# base 5 sus valores son de 0 a 4
+a = int('12222',5)
+print(f"a= a base 5:{a}")
+
+
