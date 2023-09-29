cf8261af031ec80be7a55d71164a332292779387
Federico Salinas <fede_ms@outlook.es>
2023-09-29 20:48:34 -0300

    Video 3 y 4 clase 6
    Avanzamos con python

--- Laboratorio IV/Python/Semana15_cuarto_semestre/Leccion05/Clase01/dar_formato.py
+++ Laboratorio IV/Python/Semana15_cuarto_semestre/Leccion05/Clase01/dar_formato.py
@@ -3,9 +3,22 @@
 nombre = "Ariel"
 edad = 28
 mensaje_con_formato = "Mi nombre es %s y tengo %d años" % (nombre, edad)
-print(mensaje_con_formato)
+#print(mensaje_con_formato)
 
 # Creamos uan tupla
 persona = ("Carla", "Gomes", 5000.00)
 mensaje_con_formato = "Hola %s %s. Tu sueldo es %.2f" # % persona # Aqui le pasamos el objeto que es tupla
-print(mensaje_con_formato % persona)
+# print(mensaje_con_formato % persona)
+
+# Trabajando con un formato estructurado
+nombre = "Juan"
+edad = 19
+sueldo = 3000
+#mensaje_con_formato = "Nombre {} Edad {} Sueldo {:.2f}" .format(nombre, edad, sueldo)
+
+mensaje = "Nombre {0} Edad {1} Sueldo {2:.2f}".format(nombre, edad, sueldo)
+print(mensaje)
+
+#se puede cambiar el orden y los indices
+mensaje2 = "Sueldo {2} Edad {1} Nombre {0} ".format(nombre, edad, sueldo)
+print(mensaje2)
