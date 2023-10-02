9a68c591b610d40db00a7cda890e22dcadc57151
Federico Salinas <fede_ms@outlook.es>
2023-10-02 11:07:58 -0300

    Clase 6 python terminada
    Video 7 y 8 clase 6

--- Laboratorio IV/Python/Semana15_cuarto_semestre/Leccion05/Clase01/f_string.py
+++ Laboratorio IV/Python/Semana15_cuarto_semestre/Leccion05/Clase01/f_string.py
@@ -3,3 +3,8 @@ edad = 28
 sueldo = 3000
 mensaje = f"Nombre {nombre} Edad {edad} Sueldo {sueldo:.2f}"
 print(mensaje)
+
+#Se le puede dar un formato con sep=""
+print(nombre, edad, sueldo, sep=", ")
+
+# que es una virtual box y para que sirve en el mundo de la programacion
