0bd54f35d2d1e536062b7df2faa636f678da9b61
Federico Salinas <fede_ms@outlook.es>
2023-10-02 10:59:35 -0300

    Avance semana 6 python
    Video 5 y 6 clase 6

--- Laboratorio IV/Python/Semana15_cuarto_semestre/Leccion05/Clase01/dar_formato.py
+++ Laboratorio IV/Python/Semana15_cuarto_semestre/Leccion05/Clase01/dar_formato.py
@@ -17,8 +17,15 @@ sueldo = 3000
 #mensaje_con_formato = "Nombre {} Edad {} Sueldo {:.2f}" .format(nombre, edad, sueldo)
 
 mensaje = "Nombre {0} Edad {1} Sueldo {2:.2f}".format(nombre, edad, sueldo)
-print(mensaje)
+#print(mensaje)
 
 #se puede cambiar el orden y los indices
-mensaje2 = "Sueldo {2} Edad {1} Nombre {0} ".format(nombre, edad, sueldo)
-print(mensaje2)
+#mensaje2 = "Sueldo {2} Edad {1} Nombre {0} ".format(nombre, edad, sueldo)
+#print(mensaje2)
+
+mensaje = "Nombre {n} Edad {e} Sueldo {s:.2f}".format(n=nombre, e=edad, s=sueldo)
+#print(mensaje)
+
+diccionario = {"nombre" : "Ivan", "edad" :35, "sueldo" : 8000.00}
+mensaje = "Nombre {dic[nombre]} Edad {dic[edad]} Sueldo {dic[sueldo]:.2f}".format(dic=diccionario)
+print(mensaje)
