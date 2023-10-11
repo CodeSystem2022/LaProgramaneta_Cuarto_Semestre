832429c31721afde8f10b5569d56e230db15fcac
Jazmin-Caro <jazmincarocaceres05@gmail.com>
2023-10-11 20:15:07 -0300

    Clase 6 js seguna parte
    Final de la clase 6 de JS.

---
+++ Laboratorio IV/Javascript/e-commerce2022/PERNNstack/src/controllers/auth.controller.js
@@ -0,0 +1,7 @@
+export const singin = (req, res) => res.send("ingresando");
+
+export const singnup = (req, res) => res.send("resgistrando");
+
+export const singnout = (req, res) => res.send("Cerrando sesion");
+
+export const profile = (req, res) => res.send("Perfil de usuario")
